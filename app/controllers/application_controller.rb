e90676b97a1b53bffa49f8fe1f72a51da8012a61
Doug Bennett <dben89x@gmail.com>
2015-12-21 06:16:29 -0700

    paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    session[:previous_url] || user_root_path
+    session[:previous_url] || blog_posts_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
